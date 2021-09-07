1392058a47f625a6a2925edd3c1f973a5cee5c98
Shaheryar112 <64093183+Shaheryar112@users.noreply.github.com>
2021-09-07 18:34:42 +0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Shaheryar Ahmad Khan"));
+app.get("/", (req, res) =>
+  res.send("Shaheryar Ahmad Khan----AWS CODEPIPELINE")
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
